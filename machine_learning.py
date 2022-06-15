82bac9de837419ed947002575428e9a4c2756c8a
Luka Namačinski <luka.namacinski@gmail.com>
2022-06-15 15:33:42 +0200

    Added model for MNIST

--- machine_learning.py
+++ machine_learning.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 
 from constants import COEFFICIENTS_KEY, INTERCEPTS_KEY
 from sklearn.neural_network import MLPClassifier
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_iris, load_digits
 from sklearn.metrics import confusion_matrix
 from functools import reduce
 
@@ -89,9 +89,55 @@ class IrisClassifier:
         return len(self.model.coefs_)
 
 
-if __name__ == '__main__':
-    X, y = load_iris(return_X_y=True)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+class MNISTClassifier():
 
-    split = split_dataset(X_train, y_train, 2, 3)
-    print(split[0:3])
+    def __init__(self):
+        self.model = MLPClassifier(
+            hidden_layer_sizes=(40,),
+            activation='logistic',
+            solver='adam',
+            alpha=1e-4,
+            learning_rate='constant',
+            learning_rate_init=0.2)
+
+    def fit(self, X, y):
+        self.model.fit(X, y)
+
+    def partial_fit(self, X, y):
+        self.model.partial_fit(X, y)
+
+    def predict(self, X):
+        return self.model.predict(X)
+
+    def score(self, X, y):
+        return self.model.score(X, y)
+
+    def confusion_matrix(self, X, y_true):
+        y_predicted = self.predict(X)
+        return confusion_matrix(y_true, y_predicted)
+
+    def get_coefficients(self):
+        return self.model.coefs_
+
+    def set_coefficients(self, coefficients):
+        self.model.coefs_ = coefficients
+
+    def get_intercepts(self):
+        return self.model.intercepts_
+
+    def set_intercepts(self, intercepts):
+        self.model.intercepts_ = intercepts
+
+    def get_number_of_layers(self):
+        return len(self.model.coefs_)
+
+
+if __name__ == '__main__':
+    X, y = load_digits(return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+    model = MNISTClassifier()
+    model.fit(X_train, y_train)
+
+    print(model.score(X_test, y_test))
+    print(model.confusion_matrix(X_test, y_test))
